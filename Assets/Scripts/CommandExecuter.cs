e097d134a66fed7df9f9eb3bc378dd22fe583ad4
Germán Cruz <clz.german@gmail.com>
2016-05-28 16:54:03 -0500

    New Layer System

---
+++ Assets/Scripts/CommandExecuter.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.SerializableAttribute]
+public class Command
+{
+    public float delay;
+}
+
+[System.SerializableAttribute]
+public class TweenCommand: Command
+{
+    public float tweenTime;
+    public Vector2 tweenPosition;
+}
+
+[System.SerializableAttribute]
+public class ScaleCommand: Command
+{
+    public float scaleTime;
+    public Vector2 scaleVector;
+}
+
+[System.SerializableAttribute]
+public class ChangeSpriteCommand: Command
+{
+    public Sprite newSprite;
+}
+
+public class CommandExecuter : MonoBehaviour 
+{
+	public void ExecuteCommand(Command command)
+	{
+		if(command != null)
+		{
+			if(command is TweenCommand)
+			{
+				ExecuteTweenCommand(command as TweenCommand);
+			}
+			else if(command is ScaleCommand)
+			{
+				ExecuteScaleCommand(command as ScaleCommand);
+			}
+			else if(command is ChangeSpriteCommand)
+			{
+				ExecuteChangeSpriteCommand(command as ChangeSpriteCommand);
+			}
+		}
+	}
+	
+	private void ExecuteTweenCommand(TweenCommand command)
+	{
+        LeanTween.move(gameObject, command.tweenPosition, command.tweenTime).setDelay(command.delay).setEase(LeanTweenType.easeInOutQuad);;
+	}
+	
+	private void ExecuteScaleCommand(ScaleCommand command)
+	{
+        if (command.scaleVector != Vector2.zero)
+        {
+            LeanTween.scale(gameObject, command.scaleVector, command.scaleTime).setDelay(command.delay).setEase(LeanTweenType.easeInOutQuad);;
+        }
+    }
+	
+	private void ExecuteChangeSpriteCommand(ChangeSpriteCommand command)
+	{
+        if (command.newSprite != null)
+        {
+            StartCoroutine(WaitForChangeSprite(command));
+        }
+    }
+
+    private IEnumerator WaitForChangeSprite(ChangeSpriteCommand command)
+    {
+        yield return new WaitForSeconds(command.delay);
+        gameObject.GetComponent<SpriteRenderer>().sprite = command.newSprite;
+    }
+}
---
+++ Assets/Scripts/LayerEvents.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EventTrigger
+{
+    onMoveForward,
+    onMoveBackward
+}
+
+[System.SerializableAttribute]
+public class CommandList
+{
+    public ObjectCommand[] commandArray;
+}
+
+[System.SerializableAttribute]
+public class ObjectCommand
+{
+    public GameObject goTarget;
+    public TweenCommand tweenCommand;
+    public ScaleCommand scaleCommand;
+    public ChangeSpriteCommand changeSpriteCommand;
+}
+
+public class LayerEvents : MonoBehaviour
+{
+    public CommandList onMoveForwardCommandList;
+    public CommandList onMoveBackwardCommandList;
+
+    public void ExecuteCommandList(EventTrigger trigger)
+    {
+        CommandList list = GetListToExecute(trigger);
+        
+        if(list != null)
+        {
+            foreach (ObjectCommand objCommand in list.commandArray)
+            {
+                objCommand.goTarget.GetComponent<CommandExecuter>().ExecuteCommand(objCommand.tweenCommand);
+                objCommand.goTarget.GetComponent<CommandExecuter>().ExecuteCommand(objCommand.scaleCommand);
+                objCommand.goTarget.GetComponent<CommandExecuter>().ExecuteCommand(objCommand.changeSpriteCommand);
+            }
+        }
+    }
+    
+    private CommandList GetListToExecute(EventTrigger trigger)
+    {
+        CommandList list = null;
+        
+        if(trigger == EventTrigger.onMoveForward)
+        {
+            list = onMoveForwardCommandList;
+        }
+        else if(trigger == EventTrigger.onMoveBackward)
+        {
+            list = onMoveBackwardCommandList;
+        }
+        
+        return list;
+    }
+}
